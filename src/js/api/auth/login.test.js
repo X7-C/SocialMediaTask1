41f0f9ac6b0e35314d17ccd316c79085180fcb90
Max <ussrspartanx@gmail.com>
2024-10-19 21:21:16 +0200

    Successfully testing the login function with a pass. Using fakeToken for overall better security and reliability in a test.

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,5 +1,26 @@
-import login from './login';
+import { login } from './login';
+import { save, load } from '../../storage/index';
 
-test('login function works properly', () => {
-  expect(login('makbat00180@stud.noroff.no', 'Tester1234')).toBe();
+jest.mock('../../storage/index', () => ({
+  save: jest.fn(),
+  load: jest.fn(),
+}));
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ accessToken: 'fakeToken', name: 'Mak Bat' }),
+  })
+);
+
+test('login function saves token and profile', async () => {
+  load.mockReturnValue('fakeToken');
+
+  const email = 'makbat00180@stud.noroff.no';
+  const password = 'Tester1234';
+
+  await login(email, password);
+
+  expect(save).toHaveBeenCalledWith('token', 'fakeToken');
+  expect(save).toHaveBeenCalledWith('profile', { name: 'Mak Bat' });
 });
