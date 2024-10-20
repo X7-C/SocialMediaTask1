458c62fe852fc9e527b686c80c1f39ec4831daf3
Max <ussrspartanx@gmail.com>
2024-10-20 20:34:32 +0200

    Succesful login via Cypress with valid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,15 +1,35 @@
-// Enter email and password
-// submit the form
-// check if token is saved in localStorage
-
 describe('Login Tests', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/');
   });
 
   it('User can log in with valid credentials', () => {
-    cy.get('#loginEmail').type('makbat00180@stud.noroff.no', { delay: 100 });
-    cy.get('#loginPassword').type('Tester1234', { delay: 100 });
+    cy.get('div.modal')
+      .should('be.visible')
+      .then((modal) => {
+        cy.wrap(modal)
+          .find('button[data-bs-dismiss="modal"]')
+          .first()
+          .click({ force: true });
+      });
+
+    cy.wait(500);
+    cy.get('button[data-auth="login"]')
+      .filter((index, button) => {
+        return button.innerText.trim() === 'Login';
+      })
+      .first()
+      .click({ force: true });
+
+    cy.wait(500);
+    cy.get('#loginEmail', { timeout: 8000 })
+      .should('be.visible')
+      .type('makbat00180@stud.noroff.no', { delay: 100 });
+
+    cy.get('#loginPassword')
+      .should('be.visible')
+      .type('Tester1234', { delay: 100 });
+
     cy.get('#loginForm').submit();
   });
 });
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import js from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+        cy: true,
+        Cypress: true,
+      },
+    },
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
 ];
